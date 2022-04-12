f8039a993be3f8cbd18c3b9e4bf5caf925eb50fc
AVIADAR <codingame222@gmail.com>
2022-04-12 15:40:36 +0300

    added data_exploration.ipynb

--- utilities/sentence_utility.py
+++ utilities/sentence_utility.py
@@ -18,7 +18,7 @@ class Transform(Enum):
 
 
 class SentenceUtil:
-    def __init__(self, input_txt_list, save_input=False, module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"):
+    def __init__(self, input_txt_list, save_input=False, module_url="https://tfhub.dev/google/universal-sentence-encoder-large/5"):
         # @param ["https://tfhub.dev/google/universal-sentence-encoder/4", "https://tfhub.dev/google/universal-sentence-encoder-large/5"]
         # module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
         self.similarity = None
